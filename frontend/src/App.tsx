f204b5e65f40b06f849f74eff1fe61341c785244
DavidCs9 <davidcastrosi@hotmail.com>
2025-04-10 16:54:30 -0600

    Enhance App component to handle loading and error states in data fetching with trpc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,19 @@ import { useQuery } from "@tanstack/react-query";
 import { trpc } from "./utils/trpc";
 
 function App() {
-  const query = useQuery(trpc.general.hello.queryOptions("Alecita"));
+  const { data, isLoading, isError } = useQuery(
+    trpc.general.hello.queryOptions("Alecita")
+  );
 
-  return <div>{query.data}</div>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return <div>{data}</div>;
 }
 
 export default App;
