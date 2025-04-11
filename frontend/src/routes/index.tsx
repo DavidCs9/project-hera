b3930402ee482efabe241d88d170ec4970f0c726
DavidCs9 <davidcastrosi@hotmail.com>
2025-04-11 16:49:42 -0600

    Refactor Index component layout by removing the header and adjusting the height calculation for better responsiveness. This change streamlines the UI structure while maintaining the existing gradient background.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -12,10 +12,7 @@ function Index() {
   const [currentForm, setCurrentForm] = useState<FormType>("pacientes");
 
   return (
-    <div className="flex flex-col items-center h-[calc(100vh-59px)] bg-gradient-to-b from-blue-100 to-blue-200 p-10 gap-10 ">
-      <header className="w-full text-center">
-        <h1 className="text-2xl font-bold">Proyecto Doctor Otter</h1>
-      </header>
+    <div className="flex flex-col items-center h-[calc(100vh-0px)] bg-gradient-to-b from-blue-100 to-blue-200 p-10 gap-10 ">
       <div className="flex flex-row gap-4 w-full md:max-w-5xl md:mx-auto">
         <Sidebar currentForm={currentForm} onFormChange={setCurrentForm} />
         <FormSwitcher currentForm={currentForm} />
