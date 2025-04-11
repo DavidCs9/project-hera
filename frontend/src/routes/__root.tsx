391141ed4c2e59eee901793a2f2168ee23831d4f
DavidCs9 <davidcastrosi@hotmail.com>
2025-04-11 17:12:48 -0600

    Update header in root route to include 'items-center' class for improved vertical alignment of elements.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="w-full text-center flex flex-row justify-between px-8 py-2  bg-gradient-to-b from-blue-100 to-blue-200">
+      <header className="w-full text-center flex flex-row justify-between px-8 py-2  bg-gradient-to-b from-blue-100 to-blue-200 items-center">
         <h1 className="text-2xl font-bold">Project Hera</h1>
         <div className="flex flex-row gap-2  justify-center">
           <Link to="/" className="[&.active]:font-bold">
