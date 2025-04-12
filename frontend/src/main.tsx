7bfabc951f282611a02d7d22f20bf63b06c6d6df
DavidCs9 <davidcastrosi@hotmail.com>
2025-04-12 13:07:12 -0600

    Add React Query Devtools for enhanced debugging
    - Included `@tanstack/react-query-devtools` in `package.json` to facilitate better debugging of React Query operations.
    - Updated `pnpm-lock.yaml` to reflect the addition of the new devtools package.
    - Integrated `ReactQueryDevtools` component in `main.tsx` to enable devtools in the application.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./utils/trpc.ts";
 import { Toaster } from "sonner";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
@@ -23,6 +24,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
